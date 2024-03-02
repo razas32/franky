4cad26bbb1a1fc2a0354c139ab02d8181c3be07d
unknown <razas32@mcmaster.ca>
2024-03-02 07:33:57 -0500

    Only linear driving capabilities

---
+++ main.py
@@ -0,0 +1,11 @@
+# This file is for strategy
+
+from util.objects import *
+from util.routines import *
+
+
+class Bot(GoslingAgent):
+    # This function runs every in-game tick (every time the game updates anything)
+    def run(self):
+        # set_intent tells the bot what it's trying to do
+        self.set_intent(drive(6000))
